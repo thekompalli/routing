ab86c0ed8032452c2609979267f4bf4dcf71df8b
Krishna Kompalli <kompalligk@gmail.com>
2020-05-29 10:55:06 +0530

    done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+
+
+const routes: Routes = [{
+  path:"",
+  component:HomeComponent
+},{
+  path:"about",
+  component:AboutComponent
+},{
+  path:"blog/:id",
+  component:BlogComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
